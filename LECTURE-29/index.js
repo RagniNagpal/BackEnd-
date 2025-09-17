2743800055941b9568d838bea90be8c53fcf5b21
RagniNagpal <ragninagpal@gmail.com>
2025-09-17 20:34:22 +0530

    LEC 29 and 30

--- LECTURE-29/index.js
+++ LECTURE-29/index.js
@@ -2,7 +2,7 @@ const { PrismaClient } = require('./generated/prisma');
 const prisma = new PrismaClient()
 
 async function addUser(name, email, password) {
-  const newUser = await prisma.user.create({
+  let newUser = await prisma.user.create({
     data: {
       name: name,
       email: email,
@@ -11,19 +11,20 @@ async function addUser(name, email, password) {
   });
   return newUser; 
 }
-// addUser("ragniii","rag@gmail.com","12")
-// .then(()=>{console.log("user added successfully")})
+// addUser("vanshika","van@gmail.com","25")
+// .then(()=>{console.log("user3 added successfully")})
 
 async function addTweet(content,userId){
   await prisma.tweet.create({
     data:{
       content:content,
-      userId:userId
+      userId:Number(userId)
     }
   })
 }
-// addTweet("my first tweet",1)
+// addTweet("my eleventh tweet", "11")
 // .then(()=>console.log("tweet is created"))
+
 
 
 //find all tweet by user whose id=1;
@@ -86,5 +87,74 @@ async function deleteTweet(tweetid,userId){
     }
   })
 }
-deleteTweet("1","1")
-.then(()=>console.log("tweet is deleted"));
+// deleteTweet("1","1")
+// .then(()=>console.log("tweet is deleted"));
+
+// const express=require('express')
+// const app=express()
+// app.use(express.json());
+// app.use(express.urlencoded({extended:true}));
+// const {addUser,getUsers,getUser,updateUser,deleteUser,addTweet,findTweet,updateTweet,deleteTweet}=require("./Controller/index.js")
+// app.post('/addUser',addUser);
+// app.get('/getUsers',getUsers);
+// app.get('/getUser/:id',getUser);
+// app.put("/updateUser/:id",updateUser);
+// app.delete("/deleteUser/:id",deleteUser);
+// app.post('/addTweet',addTweet);
+// app.get("/findTweet/:userId",findTweet);
+// app.put("/updateTweet",updateTweet);
+// app.delete("/deleteTweet",deleteTweet);
+// app.listen(3000,()=>{
+//     console.log("Server started")
+// })
+
+
+//create a function to delete user byid;
+
+
+// app.delete("/deleteUser/:id",deleteUser);
+// hamko pahele sare tweets delete karne padenge phir user delete hoga=> foreign key constraint
+async function deleteUser(id){
+  let user=await prisma.user.delete({
+    where:{
+      id:Number(id),
+    }
+  })
+  return user;
+}
+// deleteUser("1")
+// .then((data)=>console.log(data))
+// .catch(err=>console.log(err))
+
+async function userData(){
+  let data=await prisma.user.findMany({
+    select:{
+      id: true, 
+      name:true,
+      email:true
+    },
+  })
+  return data;
+}
+// userData().then((data)=>console.log(data))
+// .catch(err=>console.log(err));
+
+
+async function usersWithTweets() {
+  let data = await prisma.user.findMany({
+    select: {
+      name: true,
+      email: true,
+      tweet: {
+        select: { content: true }
+      }
+    }
+  })
+  return data;
+}
+usersWithTweets()
+.then ((data) => {
+    console.log(JSON.stringify(data, null, 2));
+})
+  
+
